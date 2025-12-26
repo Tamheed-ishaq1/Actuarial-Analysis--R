f46d1741ea644a774ca5a1eda493ba4d95ff3d27
Tamheed-ishaq1 <tamheedpc@gmail.com>
2025-12-26 15:37:56 +0530

    updated

--- Weibull Parametric Regression.R
+++ Weibull Parametric Regression.R
@@ -1,19 +1,19 @@
-###############################################
+
 # LAB: Weibull Parametric Regression
 # Dataset: lung (survival package)
-###############################################
+
 
 library(survival)
 library(flexsurv)
 
 # Load and clean dataset
-data("lung")
+lung
 lung$event <- ifelse(lung$status == 2, 1, 0)  # convert to 0/1 event
 
 head(lung)
 
 # Fit Weibull regression model
-fit_weib <- flexsurvreg(Surv(time, event) ~ age + sex + ph.ecog,
+fit_weib <- flexsurvreg(Surv(time,status) ~ age + sex + ph.ecog,
                         data = lung,
                         dist = "weibull")
 
@@ -27,12 +27,16 @@ plot(fit_weib, type = "survival", main = "Weibull Survival Curve")
 plot(fit_weib, type = "hazard", main = "Weibull Hazard Curve")
 
 # Predictions for selected profiles
-newdata <- data.frame(age = c(50, 70),
-                      sex = c(1, 2),
+#If you want survival for a specific REAL person
+summary(fit_weib,
+        newdata = data.frame(age = 62, sex= 1, ph.ecog = 1))
+#for more than 1 patients
+patients<- data.frame(age = c(50, 70),
+                      sex = c(1,2),       #1= male,2=female
                       ph.ecog = c(1, 2))
 
-predict(fit_weib, newdata = newdata, type = "survival", t = c(100, 300, 600))
+summary(fit_weib, newdata = patients, t = c(100, 300, 600))
 
 # Extract model parameters
-fit_weib$coefficients
+;fit_weib$coefficients
 fit_weib$res
